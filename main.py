30cb9ebd322a9859a08eeb6208d729ea4c2f5628
Ali <alistair.mackinnon@outlook.com>
2020-06-13 16:53:25 +0100

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib
+import pprint as pp
 
 avo_df = pd.read_csv('/home/ali/datasets/avocado.csv')
 albany_df = avo_df[avo_df['region'] == 'Albany']
@@ -7,15 +8,19 @@ albany_df = avo_df[avo_df['region'] == 'Albany']
 # get values to iterate through
 dates = albany_df['Date'].unique()
 avo_types = albany_df['type'].unique()
+transposed_dict = {}
 
 # iterate through tp get all data
 for date in dates:
     df = albany_df[albany_df['Date'] == date]
     for avo_type in avo_types:
+        print(avo_type)
         df = albany_df[albany_df['Date'] == date]
         df = df[df['type'] == avo_type]
         avg_price = df.iloc[0]['AveragePrice']
-        print(date, avg_price, avo_type)
+        transposed_dict[date] = {'price': avg_price, 'Type': avo_type}
+
+pp.pprint(transposed_dict)
 
 
 
